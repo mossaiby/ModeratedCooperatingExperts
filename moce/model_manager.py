3ef0a5ba7f0d52d9f0781fe6e6b6333f90035c2d
Farshid Mossaiby <mossaiby@outlook.com>
2026-07-23 16:11:45 +0330

    Minor fix

--- moce/model_manager.py
+++ moce/model_manager.py
@@ -125,10 +125,14 @@ class ModelManager:
             {"role": "user", "content": user_prompt},
         ]
         inputs = loaded.tokenizer.apply_chat_template(
-            messages, add_generation_prompt=True, return_tensors="pt"
+            messages,
+            add_generation_prompt=True,
+            return_tensors="pt",
+            return_dict=True,
         ).to(cfg.device)
 
         merged_kwargs = {**cfg.generation_kwargs, **generation_kwargs}
-        output_ids = loaded.model.generate(inputs, **merged_kwargs)
-        new_tokens = output_ids[0][inputs.shape[-1]:]
+        output_ids = loaded.model.generate(**inputs, **merged_kwargs)
+        input_length = inputs["input_ids"].shape[-1]
+        new_tokens = output_ids[0][input_length:]
         return loaded.tokenizer.decode(new_tokens, skip_special_tokens=True).strip()
